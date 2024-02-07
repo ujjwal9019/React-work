21b5b6f5f7f7ce06b42940b6e9477777d17fd37c
ujjwal9019 <ujjwal9019@gmail.com>
2024-02-07 15:43:52 +0530

    Hooks in React

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,35 +1,35 @@
+import Chai from "./chai"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter,setCounter] = useState(15)
 
+  // let counter = 5
+  const addValue = () => {
+      console.log("Clicked" ,counter)
+      // counter = counter + 1
+      setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    console.log("Clicked" ,counter)
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+    // else{
+    //   break
+    // }
+   
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+<h2>Counter Project : {counter}</h2>
+<button onClick = {addValue}>Add Value </button>
+<br></br>
+<button onClick = {removeValue}>Decrease Value   </button>
+</>
+)
 }
 
 export default App
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,10 +1,10 @@
+import { useState } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<App></App>
+  
 )
